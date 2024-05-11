388551b67bece10393c0f4a5b2f88fc9bc313861
Sujan Adhikari <079bct086@ioepc.edu.np>
2024-05-11 18:58:27 +0545

    comment add

--- generatehash.py
+++ generatehash.py
@@ -1,5 +1,5 @@
 import hashlib
 password="suyog123"
-hash=hashlib.sha256(password.encode('utf-8'))
+hash=hashlib.sha256(password.encode('utf-8'))# hashing using sha256 alogrithm
 print(hash.hexdigest())
 #prints the hashes in the password
